852f8241c7a389b5e1bb0d0235b39f321fdd93b9
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-07-02 11:55:39 -0400

    commit tree eed9c32b16322c38c948f363ac40b4848e06ae1b

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(anagrams)
+        # match_array = []
+        anagrams.select do |anagram|
+            anagram.split("").sort == @word.split("").sort
+            # binding.pry 
+        end
+    end
+end
